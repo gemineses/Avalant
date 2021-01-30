07806c0ab92b8844a56490552f39b4ddc39a5dd2
jss <jss9960@hotmail.com>
2021-01-29 20:52:04 -0800

    added tabs

---
+++ scripts/index/main.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#avalant-index',
+    data: {
+      menuBar: {
+          overview: {
+            text : 'This is the Overview text'
+          },
+          videos: {
+            text : 'This is the Videos text'
+          },
+          aboutus: {
+            text : 'This is the About Us text'
+          },
+          patchNotes: {
+            text : 'This is the Patch Notes text'
+          }
+      }
+    }
+  })
